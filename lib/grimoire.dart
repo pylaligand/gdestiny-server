0a31470b114dd0608d941af7a58bf31b09828734
P.Y. Laligand <py.laligand@gmail.com>
2016-08-11 18:46:24 -0700

    Order grimoire leaderboard by percentage of max (#16)
    This is fair to XB players whose max is lower.

--- lib/grimoire.dart
+++ lib/grimoire.dart
@@ -12,25 +12,32 @@ import 'schema.dart';
 /// Handles requests for grimoire score.
 Future<shelf.Response> handle(shelf.Request request) async {
   final params = request.context;
+  const maxXbScore = 4980;
+  const maxPsScore = 5020;
   final content = {
-    'max-grimoire': {'xb': 4980, 'ps': 5020}
+    'max-grimoire': {'xb': maxXbScore, 'ps': maxPsScore}
   };
   final db = await connect(params[param.DATABASE_URL]);
+  List<dynamic> data;
   try {
-    content['data'] = await db
-        .query(
-            'SELECT * FROM ${Schema.TABLE_MAIN} ORDER BY ${Schema.MAIN_GRIMOIRE} DESC')
-        .map((row) {
+    data = await db.query('SELECT * FROM ${Schema.TABLE_MAIN}').map((row) {
       final columns = row.toMap();
+      final score = columns[Schema.MAIN_GRIMOIRE];
+      final onXbox = columns[Schema.MAIN_ON_XBOX];
+      final max = onXbox ? maxXbScore : maxPsScore;
+      final percentage = (1000 * score / max).round() / 10;
       return {
         'name': columns[Schema.MAIN_GAMERTAG],
-        'grimoire': columns[Schema.MAIN_GRIMOIRE],
-        'platform': columns[Schema.MAIN_ON_XBOX] ? 'xb' : 'ps'
+        'grimoire': score,
+        'platform': onXbox ? 'xb' : 'ps',
+        'percentage': percentage
       };
     }).toList();
   } finally {
     db.close();
   }
+  data.sort((a, b) => b['percentage'] - a['percentage']);
+  content['data'] = data;
   final body = JSON.encode(content);
   final headers = {'content-type': 'application/json'};
   return new shelf.Response.ok(body, headers: headers);
