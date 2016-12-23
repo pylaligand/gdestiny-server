53d6eaf235b6deae6649a3c853dc81cca8530db3
P.Y. Laligand <py.laligand@gmail.com>
2016-12-23 13:32:37 -0800

    Split tourney data into separate files (#41)

--- lib/tourney.dart
+++ lib/tourney.dart
@@ -1,6 +1,7 @@
 // Copyright (c) 2016 P.Y. Laligand
 
 import 'dart:async';
+import 'dart:convert';
 import 'dart:io';
 
 import 'package:path/path.dart' as path;
@@ -8,9 +9,19 @@ import 'package:shelf/shelf.dart' as shelf;
 
 /// Handles requests for PvP tourney data.
 Future<shelf.Response> handle(shelf.Request request) async {
-  final jsonPath = path.join(path.dirname(Platform.script.toFilePath()), '..',
-      'lib', 'tournament.json');
-  final body = new File(jsonPath).readAsStringSync();
+  final mainContent = {};
+  mainContent['teams'] = _getJson('teams.json');
+  mainContent['round-robin'] = _getJson('round_robin.json');
+  mainContent['rankings'] = _getJson('rankings.json');
+  mainContent.addAll(_getJson('definitions.json'));
+  final body = JSON.encode(mainContent);
   final headers = {'content-type': 'application/json'};
   return new shelf.Response.ok(body, headers: headers);
+}
+
+Map _getJson(String fileName) {
+  final jsonPath = path.join(path.dirname(Platform.script.toFilePath()), '..',
+      'lib', 'data', fileName);
+  final content = new File(jsonPath).readAsStringSync();
+  return JSON.decode(content);
 }
